9ba14546c423194db37d72cbb4016860969f446e
sitoyt <alfonsoperezrodriguez301@gmail.com>
2022-03-28 19:53:32 +0200

    Commit de mi segundo dia de trabajo

--- Tetris2/src/tetris2/Tetris2.java
+++ Tetris2/src/tetris2/Tetris2.java
@@ -4,6 +4,8 @@ public class Tetris2 {
         
         System.out.println("El proyector tiene alma");
         System.out.println("Hola mundo");
+        System.out.println("2 dia de trabajo ");
+        System.out.println("Prueba 2 dia");
         
     }
     
